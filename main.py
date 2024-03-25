53be35868fc6564bd5c474c5df86c2245851c8a5
unknown <ge.georgiev95@gmail.com>
2024-03-25 18:52:33 +0200

    Create one function for collecting data from difference sources

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+config = {'Vivacom': {'url': 'https://www.vivacom.bg/online/bg/shop/services/tariff-plan-category-mobile-voice-services/unlimited-max?offer=epc_emj240105094151989465',
+                    'container': {'type': 'CLASS_NAME', 'value': 'js-related-offer-container'},
+                      },
+            'A1': { 'url': 'https://www.a1.bg/a1-one-unlimited',
+             'container': {'type': 'XPATH', 'value': '//*[@id="37532f39-4464-4ceb-b3b1-a6b77775d93e"]/div[2]/div[2]/div[1]'},
+             },
+            'Yettel': {'url': 'https://shop.yettel.bg/bg/tariff_page',
+             'container': {'type': 'CLASS_NAME', 'value': 'tariffs-main-carousel'},
+             }
+}
 
 ALL_PLANS = {'Vivacom': [],
              'A1':[],
              'Yettel': [] 
-
 }
 
 
@@ -24,101 +35,92 @@ def get_driver(url):
 
     return driver
 
+def collection_data():
 
-def get_vivacom_data():
+    for telecom in config:
 
-    url = 'https://www.vivacom.bg/online/bg/shop/services/tariff-plan-category-mobile-voice-services/unlimited-max?offer=epc_emj240105094151989465'
-    driver = get_driver(url)
-    get_all_offers = driver.find_elements(By.CLASS_NAME, "js-related-offer-container")
+        data = config[telecom]
+        url = data['url']
+        driver = get_driver(url)
+        wait = WebDriverWait(driver, 10)
+        element_type = getattr(By, data['container']['type'])
+        container =  wait.until(EC.presence_of_all_elements_located((element_type, data['container']['value'])))
 
-    for element in get_all_offers:
-        plan_data = element.text.splitlines()
+        if telecom == 'A1':
+            plans= container[0].find_elements(By.XPATH, "*")
+            for i in range(1, len(plans)+1):
+                elements = driver.find_elements(By.CSS_SELECTOR, f"div:nth-child({i}) > .rounded-a1 .m-1\\.5")
+                for element in elements:
+                    driver.execute_script("arguments[0].click();", element)
 
-        ALL_PLANS['Vivacom'].append({
-            'PlanName': plan_data[0],
-            'MB': plan_data[3],
-            'MaxSpeed': plan_data[4],
-            'Minutes': plan_data[1],
-            'RoamingMB': plan_data[5],
-            'Price': float(plan_data[9].split('лв')[0])
-        })
-    driver.quit()
+            for index, element in enumerate(plans):
+                plan_data = element.text.splitlines()
+                price = float
+                if index == 0:
+                    price = float(int(plan_data[16])+ 0.01*int(plan_data[17]))
+                else:
+                    price = float(int(plan_data[12])+ 0.01*int(plan_data[13]))
+                ALL_PLANS['A1'].append({
+                    'PlanName': plan_data[0],
+                    'MB': plan_data[2].split()[0],
+                    'MaxSpeed': plan_data[3],
+                    'Minutes': plan_data[1].split()[0],
+                    'RoamingMB': plan_data[4],
+                    'Price': price
+                })
 
+            driver.quit()
 
-def get_a1_data():
+        elif telecom == 'Vivacom':
+            for element in container:
+                plan_data = element.text.splitlines()
 
-    url = 'https://www.a1.bg/a1-one-unlimited'
-    driver = get_driver(url)
+                ALL_PLANS['Vivacom'].append({
+                    'PlanName': plan_data[0],
+                    'MB': plan_data[3],
+                    'MaxSpeed': plan_data[4],
+                    'Minutes': plan_data[1],
+                    'RoamingMB': plan_data[5],
+                    'Price': float(plan_data[9].split('лв')[0])
+                })
+            driver.quit()
+        
+        elif telecom == 'Yettel':
+            container = container[0]
+            more_info = driver.find_elements(By.CLASS_NAME, "see-more")
+            max_speed = str
+            minutes = str
+            for btn in more_info:
+                driver.execute_script("arguments[0].click();", btn)
 
-    container = driver.find_element(By.XPATH, '//*[@id="37532f39-4464-4ceb-b3b1-a6b77775d93e"]/div[2]/div[2]/div[1]')
+            for index, element in enumerate(container.find_elements(By.XPATH, "*")):
+                plan_data_raw = element.text.splitlines()
+                plan_data = [item for item in plan_data_raw if item.strip()]
+                price_index = plan_data.index('Виж по-малко') + 1
+                if index == len(container.find_elements(By.XPATH, "*"))-1:
+                    max_speed = plan_data[3]
+                    minutes = plan_data[4].split()[0]
+                    roaming_mb = plan_data[13]
+                else:
+                    if index == 1:
+                        roaming_mb = plan_data[17]
+                    else:
+                        roaming_mb = plan_data[15]
+                    max_speed = plan_data[3] +'. ' + plan_data[4]
+                    minutes = plan_data[6].split()[0]
+                    
 
-    plans= container.find_elements(By.XPATH, "*")
-    for i in range(1, len(plans)+1):
-        elements = driver.find_elements(By.CSS_SELECTOR, f"div:nth-child({i}) > .rounded-a1 .m-1\\.5")
-        for element in elements:
-            driver.execute_script("arguments[0].click();", element)
+                ALL_PLANS['Yettel'].append({
+                    'PlanName': plan_data[0],
+                    'MB': plan_data[1].split()[0],
+                    'MaxSpeed': max_speed,
+                    'Minutes': minutes,
+                    'RoamingMB': roaming_mb,
+                    'Price': float(plan_data[price_index].split('лв')[0].replace(',', '.'))
+                })
 
-    for index, element in enumerate(plans):
-        plan_data = element.text.splitlines()
-        price = float
-        if index == 0:
-            price = float(int(plan_data[16])+ 0.01*int(plan_data[17]))
-        else:
-            price = float(int(plan_data[12])+ 0.01*int(plan_data[13]))
-        ALL_PLANS['A1'].append({
-            'PlanName': plan_data[0],
-            'MB': plan_data[2].split()[0],
-            'MaxSpeed': plan_data[3],
-            'Minutes': plan_data[1].split()[0],
-            'RoamingMB': plan_data[4],
-            'Price': price
-        })
-
-    driver.quit()
-
-
-def get_yettel_data():
-    url = 'https://shop.yettel.bg/bg/tariff_page'
-    driver = get_driver(url)
-
-    plans_container = driver.find_element(By.CLASS_NAME, "tariffs-main-carousel")
-    more_info = driver.find_elements(By.CLASS_NAME, "see-more")
-    max_speed = str
-    minutes = str
-    for btn in more_info:
-        driver.execute_script("arguments[0].click();", btn)
-
-    for index, element in enumerate(plans_container.find_elements(By.XPATH, "*")):
-        plan_data_raw = element.text.splitlines()
-        plan_data = [item for item in plan_data_raw if item.strip()]
-        price_index = plan_data.index('Виж по-малко') + 1
-        if index == len(plans_container.find_elements(By.XPATH, "*"))-1:
-            max_speed = plan_data[3]
-            minutes = plan_data[4].split()[0]
-            roaming_mb = plan_data[13]
-        else:
-            if index == 1:
-                roaming_mb = plan_data[17]
-            else:
-                roaming_mb = plan_data[15]
-            max_speed = plan_data[3] +'. ' + plan_data[4]
-            minutes = plan_data[6].split()[0]
-            
-
-        ALL_PLANS['Yettel'].append({
-            'PlanName': plan_data[0],
-            'MB': plan_data[1].split()[0],
-            'MaxSpeed': max_speed,
-            'Minutes': minutes,
-            'RoamingMB': roaming_mb,
-            'Price': float(plan_data[price_index].split('лв')[0].replace(',', '.'))
-        })
-
-    driver.quit()
-
+            driver.quit()
 
 if __name__ == "__main__":
-    get_vivacom_data()
-    get_a1_data()
-    get_yettel_data()
+    collection_data()
     print(ALL_PLANS)
